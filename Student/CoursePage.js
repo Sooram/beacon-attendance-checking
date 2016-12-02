a1aa9c723e5dd836238ba6f8835ee8fbb77ff653
wedding2020 <seo.young.kang92@gmail.com>
2016-12-02 17:19:09 +0900

    bug fix

--- Student/CoursePage.js
+++ Student/CoursePage.js
@@ -1,8 +1,8 @@
 var Observable = require('FuseJS/Observable');
+//Syncano course class
 var Syncano = require('syncano-js/dist/syncano.fuse.js');
 var ApiKeys = require("api-keys.js");
 var classname = "course";
-
 var connection = Syncano({
     accountKey: ApiKeys.accountKey,
     defaults: {
@@ -11,9 +11,8 @@ var connection = Syncano({
     }
 });
 var CourseObject = connection.DataObject;
-var studentId = Observable("");
-//signup page나.. 아 저장되어있는 값이 있어야 할듯. 로그인을 하면 저장되어 있는 학번으로 그 날의 코스를 불러오기.
 
+var studentId = Observable("");
 
 function getStudentId(){
 	var storage = require('FuseJS/Storage');
@@ -21,33 +20,39 @@ function getStudentId(){
 	var json = JSON.parse(userData);
 	return json["student_id"];
 }
+
 //TODO: integrating with attendance info
-function getTodaysCourse(objs){
+function getTodaysCourse(){
 	day = (new Date()).toString().substring(0,3); //get today's day
-	courses = Observable({name: "No class today"});
-	arr = [];
-	if (objs.length) {
-		objs.forEach(function(course) {
-			if(course.day.includes(day)){
-				arr.push({ //if the student has this class today
-					time: course.start_time,
-					name: course.course_name,
-					color: "#1e852f",
-					enter: "NA",
-					exit: "NA",
-					absence: course.num_absent,
-					late: "NA"
-				})
-			}
-		})
-		courses.replaceAll(arr);
-	}
+	list = Observable({name: "No class today"});
+
+	CourseObject.please().filter({"students_list":{"_contains":[getStudentId()]}}).then(function(res, raw){
+		var arr = [];
+		if (res.length) {
+			res.forEach(function(course) {
+				if(1/*course.day.includes(day)*/){
+					arr.push({ //if the student has this class today
+							time: course.start_time,
+						name: course.course_name,
+						color: "#1e852f",
+						enter: "NA",
+						exit: "NA",
+						absence: course.num_absent,
+						late: "NA"
+					});
+				}
+			});
+			//list.replaceAll(arr);
+		}})
+		.catch(function (reason) {
+    		console.log("data load error: " + reason);
+    		arr = [{name: "Couldn't load courses"}];
+  		});
 }
-CourseObject.please().filter({"students_list":{"_contains":[getStudentId()]}}).then(function(res, raw){
-        getTodaysCourse(res);
-    }).catch(function (reason) {
-        console.log("data load error: " + reason);
-    });
+getTodaysCourse();
+var courses = Observable();
+courses.replaceAll(arr);
+
 // color : green "#1e852f", "#ec0707", "#ff0"
 
 var isLoading = Observable(false);
@@ -62,7 +67,6 @@ function reloadHandler(){
 	setTimeout(endLoading, 3000);	//time should be modified
 }
 module.exports = {
-	/*student_id : student_id,*/
 	courses : courses,
 	isLoading: isLoading,
 	reloadHandler: reloadHandler
--- Student/SignUp.js
+++ Student/SignUp.js
@@ -1,120 +1,118 @@
-/*
-var instances = Observable();
-var account = Syncano({accountKey : "0f4159a2a651cdc15b9353a3f3f6069274e92d94"});
-*/  
-    var Observable = require("FuseJS/Observable");
-    var success = Observable();
-    var fail = Observable();
-    
-    var id = 0;
-    var name = Observable("");
-    var student_id = Observable("");
-    var email = Observable("");
-      
-    var Syncano = require('syncano-js/dist/syncano.fuse.js');
-    var ApiKeys = require("api-keys.js");
+var Observable = require("FuseJS/Observable");
+//Syncano student class
+var Syncano = require('syncano-js/dist/syncano.fuse.js');
+var ApiKeys = require("api-keys.js");
+var classname = "student";
+var connection = Syncano({
+  accountKey: ApiKeys.accountKey,
+  defaults: {
+    instanceName: ApiKeys.instanceName,
+    className: classname
+  }
+});
+var StudentObject = connection.DataObject;
 
-    var classname = "student";
-    var connection = Syncano({
-      accountKey: ApiKeys.accountKey,
-      defaults: {
-        instanceName: ApiKeys.instanceName,
-        className: classname
+//for token
+var storage = require('FuseJS/Storage');
+
+//check if user has been logged in
+var loggedin = Observable();
+//NOTE - need a starting page before the page with signup button
+function isloggedin(){
+  if (storage.readSync("filedb.txt").length){
+    loggedin.value = true;
+    return true;
+  } else {
+    loggedin.value = false;
+    return false;
+  }
+}
+isloggedin();
+
+//triggered when user pressed singup done button
+var success = Observable();
+var fail = Observable();
+var name = Observable("");
+var student_id = Observable("");
+var email = Observable("");
+var constraintErrMsg = Observable("");
+// When authenticated, success will be true and fail will be false. When not authenticated, vice versa.
+function authenticate() {
+  var studentObject = {
+    "name": name.value,
+    "student_id": student_id.value,
+    "email": email.value,
+  };
+  success.value = false;
+  fail.value = true;
+  
+  // Checking constraint
+  if (name.value == ""){
+    constraintErrMsg.value = "Please enter your name";
+    return false;
+  } 
+  else if (!(/20\d{2}-\d{5}$/.test(student_id.value))){
+    constraintErrMsg.value = "Please check your student id format";
+    return false;
+  }
+  else if (!(/.+@snu[.]ac[.]kr$/.test(email.value))){
+    constraintErrMsg.value = "Please check your SNU Email";
+    return false;
+  }
+  else {
+    // student id and email should be unique
+    doesExist = false;
+    StudentObject.please().filter({"student_id":{"_eq":student_id.value}})
+    .then(function(response){
+      if (response.length){
+        constraintErrMsg.value = "This student id is already registered";
+        doesExist = true;
       }
-    });
-
-    var StudentObject = connection.DataObject;
-    var constraintErrMsg = Observable("");
-
-    var storage = require('FuseJS/Storage');
-    function authenticate() {
-      // When authenticated, success will be true and fail will be false. When not authenticated, vice versa.
-
-      var studentObject = {
-        "name": name.value,
-        "student_id": student_id.value,
-        "email": email.value,
-        "verification":false
-        };
-
-      success.value = false;
-      fail.value = true;
-
-      // Checking constraint
-      if (name.value == ""){
-        constraintErrMsg.value = "Please enter your name";
-        return false;
-      } else if (!(/20\d{2}-\d{5}$/.test(student_id.value))){
-        constraintErrMsg.value = "Please check your student id format";
-        return false;
-      } else if (!(/.+@snu[.]ac[.]kr$/.test(email.value))){
-        constraintErrMsg.value = "Please check your SNU Email";
-        return false;
-      } else {
-        // student id, email, device number should be unique
-        doesExist = false;
-        StudentObject.please().filter({"student_id":{"_eq":student_id.value}}).then(function(response){
+      else {
+        StudentObject.please().filter({"email":{"_eq":email.value}}).then(function(response){
           if (response.length){
-            constraintErrMsg.value = "This student id is already registered";
+            constraintErrMsg.value = "This email is already registered";
             doesExist = true;
-            return;
-          }}).then(function(){
-            StudentObject.please().filter({"email":{"_eq":email.value}}).then(function(response){
-              if (response.length){
-                constraintErrMsg.value = "This email is already registered";
-                doesExist = true;
-                return;
-              }
-            });
-          }).then(function(){
-            if (doesExist){
-              return;
-            } else {
-              // If given data is unique and well-formed, put it in db
-              StudentObject.please().create(studentObject).then(function (response, raw) {
-                console.log("Added class " + JSON.stringify(response)); 
-                success.value = true;
-                fail.value = false;
-                storage.write("filedb.txt", JSON.stringify(response)); //write to file
-                }).catch(function (reason) {
-                  console.log("forceClass create error: " + reason);
-                }).then(function(){
-                  loggedin.value = true;
-                  name.value = "";
-                  student_id.value = "";
-                  email.value = "";
-                });
-              return;
-            }
-            //TODO: Fix Bug) It shows "Try Again!"" even when it's succeeded
-          })
+          }});
       }
-    }
-    var loggedin = Observable();
-    //NOTE - need a starting page before the page with signup button
-    //check if user has been logged in
-    function isloggedin(){
-      if (storage.readSync("filedb.txt").length){
-        loggedin.value = true;
-        return true;
-      } else {
-        loggedin.value = false;
-        return false;
+    })
+    .then(function(){
+      if (!doesExist){
+        // If given data is unique and well-formed, put it in db
+        StudentObject.please().create(studentObject).then(function (response, raw) {
+          //console.log("Added class " + JSON.stringify(response)); 
+          storage.write("filedb.txt", JSON.stringify(response)); //write to file
+        })
+        .catch(function (reason) {
+          console.log("forceClass create error: " + reason);
+        })
+        .then(function(){
+          loggedin.value = true;
+          success.value = true;
+          fail.value = false;
+          name.value = "";
+          student_id.value = "";
+          email.value = "";
+        });
+        return;
       }
-    }
-    isloggedin();
+    //TODO: Fix Bug) It shows "Try Again!" even when it's succeeded
+    })
+  }
+}
 
-    module.exports = {
-      authenticate: authenticate,
-      success: success,
-      fail: fail,
-      constraintErrMsg: constraintErrMsg,
-      name: name,
-      student_id: student_id,
-      email: email,
-      isloggedin: isloggedin,
-      loggedin: loggedin
-    };
+
+module.exports = {
+  authenticate: authenticate,
+  success: success,
+  fail: fail,
+  constraintErrMsg: constraintErrMsg,
+  name: name,
+  student_id: student_id,
+  email: email,
+  isloggedin: isloggedin,
+  loggedin: loggedin
+};
     /*
 function genUniqueName() {
     var adjs = [
